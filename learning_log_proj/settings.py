068820e16a7baf0d3483f2b5cfb4df0372dcf24c
tacodes <64085493+tim-andes@users.noreply.github.com>
2020-10-16 11:34:07 -0400

    rm secret key

--- learning_log_proj/settings.py
+++ learning_log_proj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9t*@m27+7kwrrsi@vpg7x52t$=u+^oj(%f9nq8tw-hq=4p!j0w'
+SECRET_KEY = # REMOVED
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
